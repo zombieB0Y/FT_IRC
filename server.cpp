56a83f4620cc7891e11bc43a0c460112ecf1dafb
zombieBOY <zoubairgaming5@gmail.com>
2026-04-02 15:32:14 +0100

    starting

--- server.cpp
+++ server.cpp
@@ -15,23 +15,39 @@ int main() {
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(8080);
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	if (bind(servsocket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+	int optval = 1;
+	if (setsockopt(servsocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1
+		|| bind(servsocket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
 		std::cerr << "binding server socket failed !\n";
+		close(servsocket);
 		return 1;
 	}
 	if (listen(servsocket, 5) == -1) {
 		std::cerr << "listening server socket failed !\n";
+		close(servsocket);
 		return 1;
 	}
 	
 	int clientsocket = accept(servsocket, 0, 0);
 	char	buffer[1024];
 	// char	line[1025];
-	std::cout << "CLIENT Message >>> ";
+	// std::cout << "CLIENT Message >>> ";
 	int bit_read = recv(clientsocket, buffer, sizeof(buffer), 0);
-	while (bit_read) {
+	if (bit_read == -1) {
+		std::cerr << "recv() failed !\n";
+		close(servsocket);
+		close(clientsocket);
+		return 1;
+	}
+	while (bit_read > 0) {
 		write(1, buffer, bit_read);
 		bit_read = recv(clientsocket, buffer, sizeof(buffer), 0);
+		if (bit_read == -1) {
+			std::cerr << "recv() failed !\n";
+			close(servsocket);
+			close(clientsocket);
+			return 1;
+		}
 	}
 	close(servsocket);
 	close(clientsocket);
